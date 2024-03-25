14ced7378e3a0ff361d64870f415c6fd2a1d73d2
Ben Brochtrup <ben.brochtrup@gmail.com>
2024-03-24 20:32:56 -0500

    Added hero component to pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,12 @@
-import Equalizer from "@/components/Equalizer";
-import SpotifyRecent from "@/components/SpotifyRecent";
+import Hero from '@/components/Hero'
 
 export default function Projects() {
-  return <main>
-  </main>
+  return (
+    <main>
+      <Hero
+        title="Contact"
+        subtitle="Here are some ways you can get in touch"
+      />
+    </main>
+  )
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React, { useState } from 'react'
+
+interface HeroProps {
+  title: string
+  subtitle: string
+}
+
+const Hero: React.FC<HeroProps> = ({ title, subtitle }) => {
+  return (
+    <div className="w-full h-min m-4">
+      <div className="text-9xl text-center p-2">{title}</div>
+      <div className="text-3xl text-center p-2">{subtitle}</div>
+    </div>
+  )
+}
+
+export default Hero
